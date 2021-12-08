6aac9a4216c5a6209e90e52c79193915096ae0ab
Sergei Terentev <sergei.terentev@aspose.com>
2021-12-08 08:45:42 +0300

    Updating samples for v21.12

---
+++ Examples/GroupDocs.Merger.Cloud.Examples.CSharp/GetLicenseConsumption.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using GroupDocs.Merger.Cloud.Sdk.Api;
+
+namespace GroupDocs.Merger.Cloud.Examples.CSharp
+{
+    /// <summary>
+    /// This example demonstrates how to get metered license consumption information
+    /// </summary>
+    public class GetLicenseConsumption
+    {
+        public static void Run()
+        {
+            var apiInstance = new LicenseApi(Common.GetConfig());
+
+            try
+            {
+                var response = apiInstance.GetConsumptionCredit();
+
+                Console.WriteLine($"Credits: {response.Credit}");
+                Console.WriteLine($"Quantity: {response.Quantity}");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception while calling LicenseApi: " + e.Message);
+            }
+        }
+    }
+}
